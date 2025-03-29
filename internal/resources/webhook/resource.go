1d6ba33da206ea534e201b7bec6f3cf74050736b
Michael van Tellingen <m.vantellingen@labdigital.nl>
2025-03-29 10:50:31 +0100

    chore: improve webhook resource code

--- internal/resources/webhook/resource.go
+++ internal/resources/webhook/resource.go
@@ -3,14 +3,13 @@ package webhook
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"strings"
 
-	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	"github.com/labd/terraform-provider-contentful/internal/sdk"
@@ -111,7 +110,7 @@ func (e *webhookResource) Create(ctx context.Context, request resource.CreateReq
 	draft := plan.DraftForCreate()
 
 	resp, err := e.client.CreateWebhookWithResponse(ctx, plan.SpaceId.ValueString(), draft)
-	if err != nil {
+	if err := utils.CheckClientResponse(resp, err, http.StatusCreated); err != nil {
 		response.Diagnostics.AddError(
 			"Error creating webhook",
 			"Could not create webhook: "+err.Error(),
@@ -119,19 +118,13 @@ func (e *webhookResource) Create(ctx context.Context, request resource.CreateReq
 		return
 	}
 
-	if resp.StatusCode() != 201 {
-		response.Diagnostics.AddError(
-			"Error creating webhook",
-			fmt.Sprintf("Received unexpected status code: %d", resp.StatusCode()),
-		)
-		return
-	}
-
 	// Map response to state
-	plan.Import(resp.JSON201)
+	state := &Webhook{}
+	state.Import(resp.JSON201)
+	state.HttpBasicAuthPassword = plan.HttpBasicAuthPassword
 
 	// Set state
-	response.Diagnostics.Append(response.State.Set(ctx, plan)...)
+	response.Diagnostics.Append(response.State.Set(ctx, state)...)
 }
 
 func (e *webhookResource) Read(ctx context.Context, request resource.ReadRequest, response *resource.ReadResponse) {
@@ -143,7 +136,18 @@ func (e *webhookResource) Read(ctx context.Context, request resource.ReadRequest
 		return
 	}
 
-	e.doRead(ctx, &state, &response.State, &response.Diagnostics)
+	resp, err := e.client.GetWebhookWithResponse(ctx, state.SpaceId.ValueString(), state.ID.ValueString())
+	if err := utils.CheckClientResponse(resp, err, http.StatusOK); err != nil {
+		if resp.StatusCode() == 404 {
+			response.State.RemoveResource(ctx)
+			return
+		}
+		response.Diagnostics.AddError("Error reading webhook", err.Error())
+	}
+
+	state.Import(resp.JSON200)
+
+	response.Diagnostics.Append(request.State.Set(ctx, &state)...)
 }
 
 func (e *webhookResource) Update(ctx context.Context, request resource.UpdateRequest, response *resource.UpdateResponse) {
@@ -175,8 +179,7 @@ func (e *webhookResource) Update(ctx context.Context, request resource.UpdateReq
 		params,
 		draft,
 	)
-
-	if err != nil {
+	if err := utils.CheckClientResponse(resp, err, http.StatusOK); err != nil {
 		response.Diagnostics.AddError(
 			"Error updating webhook",
 			"Could not update webhook: "+err.Error(),
@@ -184,23 +187,13 @@ func (e *webhookResource) Update(ctx context.Context, request resource.UpdateReq
 		return
 	}
 
-	if resp.StatusCode() != 200 {
-		response.Diagnostics.AddError(
-			"Error updating webhook",
-			fmt.Sprintf("Received unexpected status code: %d", resp.StatusCode()),
-		)
-		return
-	}
+	state.Import(resp.JSON200)
+	state.HttpBasicAuthPassword = plan.HttpBasicAuthPassword
 
-	// Map response to state
-	plan.Import(resp.JSON200)
-
-	// Set state
-	response.Diagnostics.Append(response.State.Set(ctx, plan)...)
+	response.Diagnostics.Append(response.State.Set(ctx, state)...)
 }
 
 func (e *webhookResource) Delete(ctx context.Context, request resource.DeleteRequest, response *resource.DeleteResponse) {
-	// Get current state
 	var state Webhook
 	response.Diagnostics.Append(request.State.Get(ctx, &state)...)
 	if response.Diagnostics.HasError() {
@@ -219,19 +212,10 @@ func (e *webhookResource) Delete(ctx context.Context, request resource.DeleteReq
 		state.ID.ValueString(),
 		params,
 	)
-
-	if err != nil {
+	if err := utils.CheckClientResponse(resp, err, http.StatusNoContent); err != nil {
 		response.Diagnostics.AddError(
 			"Error deleting webhook",
 			"Could not delete webhook: "+err.Error(),
-		)
-		return
-	}
-
-	if resp.StatusCode() != 204 {
-		response.Diagnostics.AddError(
-			"Error deleting webhook",
-			fmt.Sprintf("Received unexpected status code: %d", resp.StatusCode()),
 		)
 		return
 	}
@@ -248,45 +232,19 @@ func (e *webhookResource) ImportState(ctx context.Context, request resource.Impo
 		return
 	}
 
-	futureState := &Webhook{
-		ID:      types.StringValue(idParts[0]),
-		SpaceId: types.StringValue(idParts[1]),
-	}
+	id := idParts[0]
+	spaceId := idParts[1]
 
-	e.doRead(ctx, futureState, &response.State, &response.Diagnostics)
-}
-
-func (e *webhookResource) doRead(ctx context.Context, webhook *Webhook, state *tfsdk.State, d *diag.Diagnostics) {
-	resp, err := e.client.GetWebhookWithResponse(ctx, webhook.SpaceId.ValueString(), webhook.ID.ValueString())
-	if err != nil {
-		d.AddError(
+	resp, err := e.client.GetWebhookWithResponse(ctx, spaceId, id)
+	if err := utils.CheckClientResponse(resp, err, http.StatusOK); err != nil {
+		response.Diagnostics.AddError(
 			"Error reading webhook",
 			"Could not read webhook: "+err.Error(),
 		)
 		return
 	}
 
-	// Handle 404 Not Found
-	if resp.StatusCode() == 404 {
-		d.AddWarning(
-			"Webhook not found",
-			fmt.Sprintf("Webhook %s in space %s was not found, removing from state",
-				webhook.ID.ValueString(), webhook.SpaceId.ValueString()),
-		)
-		return
-	}
-
-	if resp.StatusCode() != 200 {
-		d.AddError(
-			"Error reading webhook",
-			fmt.Sprintf("Received unexpected status code: %d", resp.StatusCode()),
-		)
-		return
-	}
-
-	// Map response to state
-	webhook.Import(resp.JSON200)
-
-	// Set state
-	d.Append(state.Set(ctx, webhook)...)
+	state := &Webhook{}
+	state.Import(resp.JSON200)
+	response.Diagnostics.Append(response.State.Set(ctx, state)...)
 }
