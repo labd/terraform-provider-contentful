568c23a1636baf61ee08ca220fb06711f0d26296
Daniel Dimovski <daniel@timeloop.se>
2025-11-11 14:11:16 +0100

    feat: additional properties in editor_interface
    Allow for arbitrary configuration in control.settings

--- internal/custommodifier/stringdefault.go
+++ internal/custommodifier/stringdefault.go
@@ -26,9 +26,9 @@ func (m stringDefaultModifier) MarkdownDescription(ctx context.Context) string {
 	return m.Description(ctx)
 }
 
-func (m stringDefaultModifier) PlanModifyString(_ context.Context, _ planmodifier.StringRequest, resp *planmodifier.StringResponse) {
-	// If the value is unknown or known, do not set default value.
-	if resp.PlanValue.IsNull() || resp.PlanValue.IsUnknown() {
+func (m stringDefaultModifier) PlanModifyString(_ context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
+	// If the value is not configured, set default value.
+	if req.ConfigValue.IsNull() {
 		resp.PlanValue = types.StringValue(m.Default)
 	}
 }
